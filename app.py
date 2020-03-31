83ec4aab6db4b2df526999f5fee5ab404d8ba4c1
Shen Ming <a6a018@yahoo.com.tw>
2020-03-31 23:31:23 +0800

    add code

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/Day1', methods=['GET'])
+@app.route('/Day1')
 def Day1():
-    return render_template('Day1/Day1.html')
+    return render_template('Day1.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
