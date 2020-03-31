1a12f20c049c900e2f2e9286a0023387452c2e43
Shen Ming <a6a018@yahoo.com.tw>
2020-03-31 23:14:39 +0800

    Flask1

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/Day1')
-def index():
+@app.route('/Day1', methods=['GET'])
+def Day1():
     return render_template('Day1/Day1.html')
 
 if __name__ == "__main__":
